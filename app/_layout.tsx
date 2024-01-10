3618d471b76b0c27372e622558c83401343b7b2a
temid22 <104782314+temid22@users.noreply.github.com>
2024-01-10 15:03:49 +0100

    OAuth Authentication

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,35 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { AntDesign, FontAwesome5, Ionicons } from '@expo/vector-icons';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { SplashScreen, Stack } from 'expo-router';
+import { SplashScreen, Stack, useRouter } from 'expo-router';
 import { useEffect } from 'react';
-import { useColorScheme } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import * as SecureStore from 'expo-secure-store';
+import { ClerkProvider, useAuth } from '@clerk/clerk-expo';
+
+const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+// Cache the Clerk JWT
+const tokenCache = {
+  async getToken(key: string) {
+    try {
+      return await SecureStore.getItemAsync(key);
+    } catch (error) {
+      return null;
+    }
+  },
+  async saveToken(key: string, value: string) {
+    try {
+      return await SecureStore.setItemAsync(key, value);
+    } catch (error) {
+      return null;
+    }
+  },
+};
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -20,8 +46,11 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
+    'Nunito-B': require('../assets/fonts/Nunito-Bold.ttf'),
+    'Nunito-EL': require('../assets/fonts/Nunito-ExtraLight.ttf'),
+    'Nunito-L': require('../assets/fonts/Nunito-Light.ttf'),
+    'Nunito-R': require('../assets/fonts/Nunito-Regular.ttf'),
+    'Nunito-SB': require('../assets/fonts/Nunito-SemiBold.ttf'),
   });
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
@@ -39,18 +68,52 @@ export default function RootLayout() {
     return null;
   }
 
-  return <RootLayoutNav />;
+  return (
+    <ClerkProvider
+      publishableKey={CLERK_PUBLISHABLE_KEY!}
+      tokenCache={tokenCache}
+    >
+      <RootLayoutNav />
+    </ClerkProvider>
+  );
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
+  // const colorScheme = useColorScheme();
+  const router = useRouter();
+  const { isLoaded, isSignedIn } = useAuth();
+
+  useEffect(() => {
+    if (isLoaded && !isSignedIn) {
+      router.push('/(modals)/login');
+    }
+  }, [isLoaded]);
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ThemeProvider>
+    <Stack>
+      <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+      <Stack.Screen
+        name='(modals)/login'
+        options={{
+          title: 'Login | Signup',
+          presentation: 'modal',
+          animation: 'fade',
+          headerTitleStyle: {
+            fontFamily: 'Nunito-SB',
+          },
+          headerTitleAlign: 'center',
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => router.back()}>
+              <AntDesign name='arrowleft' size={24} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <Stack.Screen name='listing/[id]' options={{ headerTitle: '' }} />
+      <Stack.Screen
+        name='(modals)/booking'
+        options={{ presentation: 'transparentModal' }}
+      />
+    </Stack>
   );
 }
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -2,18 +2,14 @@ const tintColorLight = '#2f95dc';
 const tintColorDark = '#fff';
 
 export default {
-  light: {
-    text: '#000',
-    background: '#fff',
-    tint: tintColorLight,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#fff',
-    background: '#000',
-    tint: tintColorDark,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorDark,
-  },
+  primary: '#9457eb',
+  grey: '#5E5D5E',
+  dark: '#1A1A1A',
+  // dark: {
+  //   text: '#fff',
+  //   background: '#000',
+  //   tint: tintColorDark,
+  //   tabIconDefault: '#ccc',
+  //   tabIconSelected: tintColorDark,
+  // },
 };
